5ee2a0e47a327ec69b27eb0803c1c18a1a6ad957
unknown <anujshashimal456@gmail.com>
2020-02-10 17:21:53 +0530

    Login Done!

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const mongo =  require('mongodb')
+
+const router = express.Router()
 require('./db/mongose')
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -46,20 +50,32 @@ app.post('/home', (req, res) => {
       .catch(err => {
           res.status(400).send("Unable to save to database");
       });
-
    });
 
-   app.post("/login", function(req, res) {
-    if(req.body.specName && req.body.specPass) {
-        if(authenticated) {
-            var token = generateAndStoreRandomString(req.body.username);
-            res.redirect("http://your.domain/path?token=" + token);
+   app.post('/redhome', function(req, res) {
+    if(req.body.specName == 'anuj' && req.body.specPass == '123') {
+            res.sendFile(__dirname + '/views/home.html')
             return;
-        }
-        // Do something if username or password wrong
+    }else{
+        res.status(400).send("Please Insert Correct Username and Password!");
     }
-    // Do something if no username or password
 });
+
+
+// router.get('/getData', function(req, res)  {
+//     const resuarray = []
+//     mongo.connect(url, function(err, db) {
+//         assert.equal(null, err)
+//         const cursor = db.collection('user').find()
+//             cursor.forEach(function(doc, err) {
+//                 assert.(null, err)
+//                 resuarray.push(doc)
+//             })
+//     })
+// })
+
+
+
 
  
 
